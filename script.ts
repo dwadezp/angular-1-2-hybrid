cd01ecb506502bef066be859221e91331430b93c
Vladyslav <vladyslav.bezruchko@cgm.com>
2018-02-13 09:50:19 +0200

    added directive

--- script.ts
+++ script.ts
@@ -7,7 +7,25 @@ angular.module("myJsApp").component("jsApp", {
     controller: function() {
       console.log("jsApp component started");
     }
-});
+})
+.directive('itemWidget', [function() {
+  return{
 
+  link : function(scope, element, attrs){
+      debugger;
+      console.log(arguments);
+      //Add event listener for 'click' event
+      element.on('click', function(event) {
+      debugger;
+              //Update DOM.						
+          element.html('Thanks for buying this item.');
+          element.css({
+          color: 'green'
+          });
+      });
+  }
+  }
+}]);
+declare const window: any;
 window.angular = angular;
 debugger;
