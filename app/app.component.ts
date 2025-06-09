810bb040c47061702a453de2cf050ac9bd29106e
JSFUN <dwadezp@gmail.com>
2025-06-09 09:57:57 +0300

    Update app.component.ts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`
+  template: `
+    <h1>{{ greeting }} {{ name }}</h1>
+    <button (click)="toggleGreeting()">Toggle Greeting</button>
+  `
 })
-export class AppComponent { name = 'Angular'; }
+export class AppComponent implements OnInit {
+  name = 'Angular';
+  hour!: number;
+  greeting!: string;
+  showTimeBasedGreeting = true;
 
+  ngOnInit() {
+    this.hour = new Date().getHours();
+    this.greeting = this.computeGreeting();
+    this.greetingDuplicate = this.computeGreetingDuplicate();
+  }
 
-/*
-Copyright 2017 Google Inc. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at http://angular.io/license
-*/
+  computeGreeting(): string {
+    // 1st decision: is it time-based? 
+    if (!this.showTimeBasedGreeting) {
+      return 'Hello';
+    }
+    // 2nd decision: morning, afternoon or evening
+    if (this.hour < 12) {
+      return 'Good morning';
+    } else if (this.hour < 18) {
+      return 'Good afternoon';
+    } else {
+      return 'Good evening';
+    }
+  }
+
+    computeGreetingDuplicate(): string {
+    // 1st decision: is it time-based? 
+    if (!this.showTimeBasedGreeting) {
+      return 'Hello';
+    }
+    // 2nd decision: morning, afternoon or evening
+    if (this.hour < 12) {
+      return 'Good morning';
+    } else if (this.hour < 18) {
+      return 'Good afternoon';
+    } else {
+      return 'Good evening';
+    }
+  }
+
+  toggleGreeting() {
+    this.showTimeBasedGreeting = !this.showTimeBasedGreeting;
+    this.greeting = this.computeGreeting();
+  }
+}
